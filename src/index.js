a73faca3997ab58fb4b7787f9eeb8d0cad41dea6
Arman920 <matevosyan.arman@mail.ru>
2022-08-04 21:33:22 +0400

    add components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/Gegham/App';
+import App from './components/Arman/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
