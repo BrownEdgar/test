0e3d97174d070d82351213c85412445e5872a739
khachikzahalyan <zahalyanxcho@gmail.com>
2022-08-09 16:17:24 +0400

    creat test JS

--- src/components/Xachik/App.js
+++ src/components/Xachik/App.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 const App = () => {
+
+/* 2.ՏՐՎԱԾ Է ՆԱԽԱԴԱՍՈՒԹՅՈՒՆ,փորձեք տպել նույն նախադասությունը բայց արդեն առաջին բառը մեծատառ սարգած */    
+
+/*     const string = "hello world";
+
+        function toUpperCase(str) {
+            const capitalize = () => str[0].toUpperCase() + str.slice(1)
+            console.log(capitalize())
+        }
+
+    toUpperCase(string) */
+
     return(
         <div>
-            asdasdasdasdasdasdasd
+            
         </div>
     )
 }
