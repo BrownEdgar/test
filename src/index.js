c9b7d4dc2bba8782bafb263935cb8a9f9809b740
Gegham831 <gegam832@gmail.com>
2022-08-13 19:58:29 +0400

    tnayin

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import App from './components/Gegham/App';
-=======
-import App from './components/Hamik/HomeWork/App';
-
->>>>>>> 2a3c7635f832f487a606ac9f9450493c4b3bb918
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
