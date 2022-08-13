b1b979719cd84f983d7ac8eada64f09236ffabe8
abc <you@example.com>
2022-08-13 16:10:26 +0400

    homework13.08

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/Xachik/App.js';
+import App from './components/Edgar/FirstExample/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
