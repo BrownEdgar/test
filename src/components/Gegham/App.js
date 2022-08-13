5265c1c714f9431cfe3f3756d18b3a4f420a3ba2
BrownEdgar <nikogosjanedgar@gmail.com>
2022-08-13 20:15:02 +0400

    add read data example

--- src/components/Gegham/App.js
+++ src/components/Gegham/App.js
@@ -5,7 +5,6 @@ import './App.css'
 
 function App() {
 	const [data, setData] = useState([])
-	
 
 	useEffect(() => {
 	fetch("https://jsonplaceholder.typicode.com/posts")
--- src/components/Xachik/Component.jsx
+++ src/components/Xachik/Component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Component = () => {
-    return(
+    return (
         <div></div>
     )
 }
