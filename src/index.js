c1effb8019255d37a2284a00a1f5469ae2aa21e9
abc <you@example.com>
2022-08-15 22:09:07 +0400

    lesson

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/Edgar/FirstExample/App';
+import App from './components/Sofi/datajson/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
