7a424a8ebd30aa30113d765cc582c587d40dcab4
abc <you@example.com>
2022-08-18 18:56:59 +0400

    sofi homework form2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/Edgar/Form/App';
+import App from './components/Sofi/Form18.08/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
