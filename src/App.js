d9b10383c40bc9e7ad520bc2f22463dca6780eef
BrownEdgar <nikogosjanedgar@gmail.com>
2022-09-06 22:00:08 +0400

    redux mas 2,3,4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { CHANGE_NAME, GET_ARR_ELEMENTS } from './components/redux/store'
+import { changeNameAction } from './components/redux/features/name'
+import {changeArrAction} from './components/redux/features/arr'
+import { addPostAction, selectPosts } from './components/redux/features/posts'
 
 
 export default function App() {
+	
 
-	const name = useSelector(state => state.name)
-	const arr = useSelector(state => state.arr)
-	const dispatch = useDispatch()
-	const filterArr = () => {
-			const f = arr.filter(elem => elem.match(/^[A-Z]/g))
-			dispatch({ type: GET_ARR_ELEMENTS,payload:{ arr:f}})
-	}
+	const posts = useSelector(selectPosts)
+
+	// const arr = useSelector(state => state.arr)
+	 const dispatch = useDispatch()
+	// const filterArr = () => {
+	// 		const f = arr.filter(elem => elem.match(/^[A-Z]/g))
+	// 	dispatch(changeArrAction(f))
+	// }
 	return (
 		<div>
-			<h1>{name}</h1>
-			<button onClick={() => dispatch({
-				type: CHANGE_NAME,
-				payload: { name: "JAVASCRIPT" }
-			})}>change redux name</button>
+			{/* <h1>{name}</h1>
+			<button onClick={() => dispatch(changeNameAction())}>change redux name</button>
 			<pre>{JSON.stringify(arr, null, 1)}</pre>
-			<button onClick={filterArr}>filter</button>
+			<button onClick={filterArr}>filter</button> */}
+			{posts.map(elem => {
+				return <pre key={elem.id}>
+					{JSON.stringify(elem, null, 1)}
+				</pre>
+			})}
+			<button onClick={() => dispatch(addPostAction())}>add post</button>
+
 
 		</div>
 	)
--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,29 +1,34 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { nameReducer, initialNameValue } from './features/name'
+import { arrReducer, initialArrValue } from './features/arr'
+import { postsReducer, initialPostValue } from './features/posts'
+import { CHANGE_NAME } from './actionTypes'
 
-export const CHANGE_NAME = 'change-name'
-export const GET_ARR_ELEMENTS = 'get-arr-elements'
 
 const initialState = {
-	name: "Redux",
-	arr:  ['html', 'css', 'Java Script', 'React.js', 'Node.js', 'Python']
+	name: initialNameValue,
+	arr: initialArrValue,
+	posts: initialPostValue,
 }
 
-function reducer(state,action) {
-		switch (action.type) {
-			case CHANGE_NAME: 
-				return { 
-					...state,
-					 name: action?.payload?.name 
-					}
-			case GET_ARR_ELEMENTS: 
-				return { 
-					...state,
-					arr: action.payload.arr
-				}
-			default: return state
-		}
+const myFirstMiddleWare = (store) => (next) => (action) => {
+	if (action.type === CHANGE_NAME) {
+		console.log("CHANGE_NAME action!");
+			if (typeof action.payload.name === "string") {
+				console.log("ok");
+				next(action)
+			}	else{
+				alert("invalid property")
+			}
+	}
+	next(action)
 }
 
-const store = createStore(reducer, initialState )
+
+const store = createStore(combineReducers({
+	name: nameReducer,
+	arr: arrReducer,
+	posts: postsReducer
+}), initialState, applyMiddleware(myFirstMiddleWare))
 
 export default store
